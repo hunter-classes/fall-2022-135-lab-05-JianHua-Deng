10be32cbe394aabd42630509f1ec82d43f0a72e4
Metails <dengjianhua2397462613@gmail.com>
2022-10-03 22:44:20 -0400

    Added command to call function in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main()
 {
 
 	std::cout << isDivisibleBy(100, 25) << "\n";
-
-  return 0;
+	std::cout << isPrime(293) << "\n";
+	std::cout << nextPrime(17) << "\n";
+	std::cout << countPrimes(16, 80) << "\n";
+	std::cout << isTwinPrime(16) << "\n";
+	std::cout << nextTwinPrime(15) << "\n";
+	std::cout << largestTwinPrime(1, 31) << "\n";
+	return 0;
 }
